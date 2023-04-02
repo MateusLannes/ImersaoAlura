2074ee8ee178a8483d46a675c1858f5b6a820879
Mateus <mateuslannes100@gmail.com>
2023-04-02 01:02:43 +0000

    Pegando Dados da API

--- src/App.java
+++ src/App.java
@@ -1,7 +1,20 @@
+import static java.net.http.HttpRequest.newBuilder;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
         String url = "https://imdb-api.com/en/API/Top250Movies/k_7kvp08yi";
-
+        URI endereco = URI.create(url);
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest request = newBuilder(endereco).GET().build();
+        HttpResponse<String> response = cliente.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
     }
 }
