592e3e0dc7bdee3e3265c01f9226649736636538
Mateus <mateuslannes100@gmail.com>
2023-04-03 21:38:49 +0000

    começando a extrair só os dados que quero

--- src/App.java
+++ src/App.java
@@ -5,10 +5,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        // conexão http, buscando top 250 filmes
         String url = "https://imdb-api.com/en/API/Top250Movies/k_7kvp08yi";
         URI endereco = URI.create(url);
         HttpClient cliente = HttpClient.newHttpClient();
@@ -16,5 +19,9 @@ public class App {
         HttpResponse<String> response = cliente.send(request, BodyHandlers.ofString());
         String body = response.body();
         System.out.println(body);
+
+        // extrair os dados que interessam
+        List<Map<String, String>> listadeFilmes;
+
     }
 }
